2b399a606b58986da82a8efd3377e3cee64ece11
saraadel6 <sa7064160@gmail.com>
2023-03-20 06:29:27 +0200

    add count sort

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include "sortlib.h"
 
 using namespace sortlib;
@@ -8,27 +9,32 @@ int main()
 {
     double numbers[] = {5.3, 2.2, 4.9, 3.23, 0.1, 10.3, 8.1};
     string names[] = {"mohamed", "mai", "ahmed", "nour", "sara", "adam", "aya"};
+    int integers[] = {5, 2, 4, 3, 0, 10, 8};
 
     int sze1 = sizeof(numbers) / sizeof(numbers[0]);
     int sze2 = sizeof(names) / sizeof(names[0]);
+    int sze3 = sizeof(integers) / sizeof(integers[0]);
 
-//    insertionSort(numbers,sze1);
-//    insertionSort(names,sze2);
+    //    insertionSort(numbers,sze1);
+    //    insertionSort(names,sze2);
 
-//    selectionSort(numbers,sze1);
-//    selectionSort(names,sze2);
+    //    selectionSort(numbers,sze1);
+    //    selectionSort(names,sze2);
 
-//    bubbleSort(numbers, sze1);
-//    bubbleSort(names, sze2);
+    //    bubbleSort(numbers, sze1);
+    //    bubbleSort(names, sze2);
 
-//    shellSort(numbers, sze1);
-//    shellSort(names, sze2);
+    //    shellSort(numbers, sze1);
+    //    shellSort(names, sze2);
 
-    mergeSort(numbers,0,sze1 - 1);
-    mergeSort(names,0,sze2 - 1);
+    mergeSort(numbers, 0, sze1 - 1);
+    mergeSort(names, 0, sze2 - 1);
 
-    print(numbers,sze1);
-    print(names,sze2);
+    print(numbers, sze1);
+    print(names, sze2);
+
+    countSort(integers, sze3);
+    print(integers, sze3);
 
     return 0;
 }
--- sortlib.h
+++ sortlib.h
@@ -4,51 +4,60 @@ namespace sortlib
 {
 
     // print array
-    template<typename T>
-    void print(T arr[], int n) {
+    template <typename T>
+    void print(T arr[], int n)
+    {
 
-        for (int i = 0 ; i < n ; i++) {
+        for (int i = 0; i < n; i++)
+        {
             std::cout << arr[i] << "  ";
         }
         std::cout << std::endl;
-
     }
 
     // swap two elements by refrence
-    template<typename T>
-    void swap(T &a, T &b) {
+    template <typename T>
+    void swap(T &a, T &b)
+    {
         T temp = a;
         a = b;
         b = temp;
     }
 
-    template<typename T>
-    void insertionSort(T arr[], int n) {
+    template <typename T>
+    void insertionSort(T arr[], int n)
+    {
 
         // loop over every element in the array starting from the second element
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++)
+        {
 
             // put every element in its sorted position on the left side
             // ( left : sorted elements | right : unsorted elements )
             int k = i;
-            while (arr[k] < arr[k - 1] && k > 0) {
+            while (arr[k] < arr[k - 1] && k > 0)
+            {
                 swap(arr[k], arr[k - 1]);
                 k--;
             }
         }
     }
 
-    template<typename T>
-    void selectionSort(T arr[], int n) {
+    template <typename T>
+    void selectionSort(T arr[], int n)
+    {
         int minIndex;
 
         // assign the current index to the minimum index
-        for (size_t i = 0; i < n - 1; i++) {
+        for (size_t i = 0; i < n - 1; i++)
+        {
             minIndex = i;
 
-            for (size_t j = i + 1; j < n; j++) {
+            for (size_t j = i + 1; j < n; j++)
+            {
                 // if there is an index less than the current minimum update the minimum index with it
-                if (arr[j] < arr[minIndex]) {
+                if (arr[j] < arr[minIndex])
+                {
                     minIndex = j;
                 }
             }
@@ -58,42 +67,89 @@ namespace sortlib
         }
     }
 
-    template<typename T>
-    void bubbleSort(T arr[], int n) {
+    template <typename T>
+    void bubbleSort(T arr[], int n)
+    {
 
         // loop through all array elements
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n - 1; i++)
+        {
 
             // last element is already sorted
-            for (int j = 0; j < n - i - 1; j++) {
+            for (int j = 0; j < n - i - 1; j++)
+            {
 
                 // swap if the element found is greater than the next element
-                if (arr[j] > arr[j + 1]) {
+                if (arr[j] > arr[j + 1])
+                {
                     swap(arr[j], arr[j + 1]);
                 }
             }
         }
     }
 
-    template<typename T>
-    void shellSort(T arr[], int n) {
+    template <typename T>
+    void shellSort(T arr[], int n)
+    {
 
         // loop for each gap , gap start from n/2 end when gap = 0
-        for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int gap = n / 2; gap > 0; gap /= 2)
+        {
 
             // loop over each element starting from gap and to the end of the array
-            for (int i = gap; i < n; i++) {
+            for (int i = gap; i < n; i++)
+            {
 
                 // put the element in the (i)th position in its correct position
-                for (int j = i; j >= gap && arr[j - gap] > arr[j]; j -= gap) {
+                for (int j = i; j >= gap && arr[j - gap] > arr[j]; j -= gap)
+                {
                     swap(arr[j - gap], arr[j]);
                 }
             }
         }
     }
 
+    void countSort(int arr[], int n)
+    {
+        // get the max element in the array
+        int max = *std::max_element(arr, arr + n);
+        // create array to store the count of each element
+        int count[max];
+        // create array to store the sorted elements
+        int sorted[n];
+
+        // initialize the count array with 0
+        for (size_t i = 0; i <= max; i++)
+        {
+            count[i] = 0;
+        }
+        // count the number of occurrences of each element and store it in the count array
+        for (int i = 0; i < n; i++)
+        {
+            count[arr[i]]++;
+        }
+        // calculate the cumulative sum of the count array
+        for (int i = 1; i <= max; i++)
+        {
+            count[i] += count[i - 1];
+        }
+        // put the elements in the sorted array in their correct position
+        for (int i = n - 1; i >= 0; i--)
+        {
+            sorted[count[arr[i]] - 1] = arr[i];
+            // decrement the count of the element after place it in the sorted array
+            count[arr[i]]--;
+        }
+        // copy the sorted array to the original array
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = sorted[i];
+        }
+    }
+
     template <typename T>
-    void merge(T arr[], int l, int m, int r){
+    void merge(T arr[], int l, int m, int r)
+    {
 
         int i, j, k;
         int n1 = m - l + 1;
@@ -113,13 +169,16 @@ namespace sortlib
         j = 0;
         k = l;
 
-        //loop through all array elements
-        while (i < n1 && j < n2) {
-            if (lArry[i] <= rArry[j]) {
+        // loop through all array elements
+        while (i < n1 && j < n2)
+        {
+            if (lArry[i] <= rArry[j])
+            {
                 arr[k] = lArry[i];
                 i++;
             }
-            else {
+            else
+            {
                 arr[k] = rArry[j];
                 j++;
             }
@@ -127,14 +186,16 @@ namespace sortlib
         }
 
         // Copy the remaining elements of Left
-        while (i < n1) {
+        while (i < n1)
+        {
             arr[k] = lArry[i];
             i++;
             k++;
         }
 
         // Copy the remaining elements of Right
-        while (j < n2) {
+        while (j < n2)
+        {
             arr[k] = rArry[j];
             j++;
             k++;
@@ -142,9 +203,11 @@ namespace sortlib
     }
 
     template <typename T>
-    void mergeSort(T arr[], int l, int r) {
+    void mergeSort(T arr[], int l, int r)
+    {
 
-        if (l < r) {
+        if (l < r)
+        {
             int m = l + (r - l) / 2;
 
             // Sort first and second halves
